f5b0f488b708443588948814bdb94a6c12841055
jnayak1 <jdn4ae@virgnia.edu>
2016-04-11 17:51:35 -0400

    change size of input text area

--- safeCollab/private_messages/forms.py
+++ safeCollab/private_messages/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class SendMessage(forms.Form):
 	content = forms.CharField(widget=forms.Textarea)
+
+	def __init__(self, *args, **kwargs):
+		super(SendMessage, self).__init__(*args, **kwargs)
+		self.fields['content'].widget.attrs['rows'] = 5
+		self.fields['content'].widget.attrs['cols'] = 90
