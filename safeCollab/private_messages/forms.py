7fdbb0bb6b21784d0352c16c610e250db3543696
Amber Lee <ajl2710@email.vccs.edu>
2016-04-12 01:11:59 -0400

    New conversation under way.

--- safeCollab/private_messages/forms.py
+++ safeCollab/private_messages/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from private_messages.models import ConversationLog
 class SendMessage(forms.Form):
 	content = forms.CharField(widget=forms.Textarea)
 
@@ -7,3 +7,8 @@ class SendMessage(forms.Form):
 		super(SendMessage, self).__init__(*args, **kwargs)
 		self.fields['content'].widget.attrs['rows'] = 5
 		self.fields['content'].widget.attrs['cols'] = 90
+
+class ConversationForm(forms.Form):
+	class Meta:
+		model = ConversationLog
+		fields = ('participants', 'log')
--- safeCollab/private_messages/urls.py
+++ safeCollab/private_messages/urls.py
@@ -3,5 +3,6 @@ from private_messages import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^new_conversation/$', views.newConversation),
     url(r'^(?P<conversationID>\w{0,50})/$', views.getConversation),
 	)
