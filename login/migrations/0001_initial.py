768cb58641ecd102f892a15527a54dd1dd0ba9f5
jnayak1 <jdn4ae@virgnia.edu>
2016-05-02 19:51:04 -0400

    Fixed bug with uploadDelete file

--- login/migrations/0001_initial.py
+++ login/migrations/0001_initial.py
@@ -14,27 +14,48 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='ActivateUser',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('username', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
-                ('name', models.CharField(max_length=128, unique=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(unique=True, max_length=128)),
                 ('views', models.IntegerField(default=0)),
                 ('likes', models.IntegerField(default=0)),
                 ('slug', models.SlugField()),
             ],
         ),
         migrations.CreateModel(
+            name='DeactivateUser',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('username', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='DeleteUser',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('username', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Groupings',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
-                ('name', models.CharField(max_length=50, unique=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('name', models.CharField(unique=True, max_length=50)),
                 ('members', models.ManyToManyField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Page',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('title', models.CharField(max_length=128)),
                 ('url', models.URLField()),
                 ('views', models.IntegerField(default=0)),
@@ -44,7 +65,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Report',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('description', models.CharField(max_length=128)),
                 ('detailed_description', models.CharField(max_length=255)),
                 ('public', models.BooleanField(default=0)),
@@ -54,19 +75,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SiteManager',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('name', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
-                ('public_key', models.CharField(max_length=100)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('public_key', models.CharField(max_length=512)),
                 ('website', models.URLField(blank=True)),
                 ('user_type', models.CharField(max_length=16)),
                 ('groups', models.ManyToManyField(related_name='groups', to='auth.Group')),
-                ('user', models.OneToOneField(related_name='user', to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, related_name='user')),
             ],
         ),
     ]
