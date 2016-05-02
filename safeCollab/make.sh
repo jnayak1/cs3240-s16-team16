011304bf6aa65911bfd2e3c06ed84ad1448c3695
jnayak1 <jdn4ae@virgnia.edu>
2016-05-01 23:37:25 -0400

    Move to TLD

--- safeCollab/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf db.sqlite3
-python manage.py syncdb
-python manage.py makemigrations
-python manage.py migrate
--- safeCollab/private_messages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from private_messages import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^new_conversation/$', views.newConversation),
-    url(r'^(?P<conversationID>\w{0,50})/$', views.getConversation),
-    url(r'^(?P<conversationID>\w{0,50})/deleteConversation$', views.deleteConversation)
-	)
